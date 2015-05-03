35f02aa116e8991ffc497a2cf39b47e1d222700d
ilyaerokhin <ilya.erokhin@gmail.com>
2015-05-03 10:21:23 +0300

    Небольшие изменения

--- Free.nn/Free.nn/MenuPage.xaml.cs
+++ Free.nn/Free.nn/MenuPage.xaml.cs
@@ -22,36 +22,9 @@ namespace Free.nn
 
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
-            //MessageBox.Show("Имя  - "+PublicData.first_name+
-            //                "\nID   - "+PublicData.id+
-            //                "\nПол  - "+PublicData.sex.ToString()+
-            //                "\nФото - "+PublicData.photo_path+
-            //                "\nToken- "+PublicData.accessToken);
-            // ОТПРАВКА НА СТЕНУ
-            //string uri = "https://api.vkontakte.ru/method/wall.post.xml?owner_id="+PublicData.id+"&message=" + "Привет" + "&access_token=" + PublicData.accessToken;
-
-            // ОТПРАВКА СООБЩЕНИЯ
-            //string uri = "https://api.vkontakte.ru/method/messages.send.xml?user_id=" + PublicData.id + "&message=" + "Привет" + "&access_token=" + PublicData.accessToken;
-
-            // СОСТОИТ ЛИ В ГРУППЕ
-            //string uri = "https://api.vkontakte.ru/method/groups.isMember.xml?group_id=" + "smmpub" + "&user_id=" + PublicData.id + "&message=" + "Привет";
-
-            // ВСТУПЛЕНИЕ В ГРУППУ
-            string uri = "https://api.vkontakte.ru/method/groups.join.xml?group_id=" + "43503600" + "&access_token=" + PublicData.accessToken;
-
-            // ВЫХОД ИЗ ГРУПП
-            //string uri = "https://api.vkontakte.ru/method/groups.leave.xml?group_id=" + "43503600" + "&access_token=" + PublicData.accessToken;
-
-            WebClient webClient = new WebClient();
-            webClient.OpenReadCompleted += wallPost_OpenReadCompleted;
-            webClient.OpenReadAsync(new Uri(uri));
+            
         }
 
-        private void wallPost_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
-        {
-            XDocument xml = XDocument.Load(e.Result);
-            //MessageBox.Show(xml.Root.Document.ToString());
-        }
 
         private void PhoneApplicationPage_BackKeyPress(object sender, System.ComponentModel.CancelEventArgs e)
         {
--- Free.nn/Free.nn/NNFreeAPI.cs
+++ Free.nn/Free.nn/NNFreeAPI.cs
@@ -16,13 +16,14 @@ namespace Free.nn
         private string p1;
         private int p2;
 
+        // конструктор
         public NNFreeAPI(string host,int port)
         {
             this.HOST = host;
             this.PORT = port;
             client = new SocketClient();
         }
-
+        // подключение
         public int Connect()
         {
             this.result = this.client.Connect(this.HOST, this.PORT);
@@ -30,20 +31,21 @@ namespace Free.nn
             if (!this.result.Contains("Success"))
             {
                 this.IsConnected = false;
-                return -1;
+                return -1; // неудача
             }
             else
             {
                 this.IsConnected = true;
-                return 0;
+                return 0; // удача
             }
         }
-
+        // закрываем соединение
         public void Close()
         {
             this.client.Close();
+            this.IsConnected = false;
         }
-
+        // получить id приложения Вконтакте
         public int App_ID()
         {
             if(this.IsConnected)
@@ -56,36 +58,301 @@ namespace Free.nn
                 }
                 else
                 {
-                    return -2;
+                    return -2; // ошибка в запросе
                 }
             }
             else
             {
-                return -1;
+                return -1; // нет соединения
             }
         }
-
+        // проверка пользователя
         public int CheckUser(string id)
         {
-            this.client.Send("<check_user/"+id+">");
-            this.result = this.client.Receive();
-            
-            if (this.result.Contains("<old_user>"))
+            if (this.IsConnected)
             {
-                return 0;
-            }
-            else
-            {
-                if(this.result.Contains("<new_user>"))
+                this.client.Send("<check_user/" + id + ">");
+                this.result = this.client.Receive();
+
+                if (this.result.Contains("<old_user>"))
                 {
-                    return 1;
+                    return 0; // старый пользователья
                 }
                 else
                 {
-                    return -1;
+                    if (this.result.Contains("<new_user>"))
+                    {
+                        return 1; // новый пользователья
+                    }
+                    else
+                    {
+                        return -2; // неизвестная ошибка
+                    }
                 }
             }
+            else
+            {
+                return -1; // нет соединения
+            }
+        }
+        // Добавление нового объявления
+        public int AddAdvert(string id, string low_text, string big_text)
+        {
+            if (this.IsConnected)
+            {
+                this.client.Send("<add_advert/" + id + "/" + low_text + "/" + big_text + ">");
+                this.result = this.client.Receive();
 
+                if (this.result.Contains("<add_advert/"))
+                {
+                    if (this.result.Contains("<add_advert/bad>"))
+                    {
+                        return -3; // ошибка в запросе
+                    }
+                    else
+                    {
+                        return Int32.Parse(this.result.Split(new Char[] { '/', '>' })[1]); // возвращаем id объявления
+                    }
+                }
+                else
+                {
+                    return -2; // неизвестная ошибка
+                }
+            }
+            else
+            {
+                return -1; // отсутствует подключение
+            }
+        }
+        // Получить объявление по id
+        public string GetAdvert(string advert_id)
+        {
+            if (this.IsConnected)
+            {
+                this.client.Send("<get_advert/" + advert_id + ">");
+                this.result = this.client.Receive();
+
+                if (this.result.Contains("<get_advert/"))
+                {
+                    if (this.result.Contains("<get_advert/bad>"))
+                    {
+                        return null; // ошибка в запросе
+                    }
+                    else
+                    {
+                        return this.result.Split(new Char[] { '/', '>' })[1] // owner_id
+                               + "/"
+                               + this.result.Split(new Char[] { '/', '>' })[2] // low_text
+                               + "/"
+                               + this.result.Split(new Char[] { '/', '>' })[3]; // big_text
+                    }
+                }
+                else
+                {
+                    return null; // неизвестная ошибка
+                }
+            }
+            else
+            {
+                return null; // отсутствует подключение
+            }
+        }
+        // получить 30 объявлений, последним из которых будет
+        // объявление с id, принимаемым параметром метода
+        public string ListAds(string advert_id)
+        {
+            if (this.IsConnected)
+            {
+                this.client.Send("<list_adverts/" + advert_id + ">");
+                this.result = this.client.Receive();
+
+                if (this.result.Contains("<list_adverts/"))
+                {
+                    string[] ads = result.Split(new Char[] { '/', '<', '>' });
+
+                    // флажок нужен для пропуска первого захода
+                    bool flag = true;
+                    string str = "";
+
+                    // в цикле заполняем список объявлений
+                    foreach (string s in ads)
+                    {
+                        // если строка не пустая
+                        if (s.Trim() != "")
+                        {
+                            // исключаем первый заход
+                            if (flag != true)
+                            {
+                                str = str + s + "/";
+                            }
+                            else
+                            {
+                                // смена флага
+                                flag = false;
+                            }
+                        }
+                    }
+                    return str; // ad1/ad2/../adn/
+                }
+                else
+                {
+                    return null; // неизвестная ошибка
+                }
+            }
+            else
+            {
+                return null; // отсутствует подключение
+            }
+        }
+        // получить количество объвлений 
+        // (или id последнего объявления)
+        public int NumberOfAds()
+        {
+            if (this.IsConnected)
+            {
+                this.client.Send("<number_of_ads>");
+                this.result = this.client.Receive();
+
+                if (this.result.Contains("number_of_ads="))
+                {
+                    return Int32.Parse(this.result.Split(new Char[] { '=', '>' })[1]); // количество объявлений в системе
+                }
+                else
+                {
+                    return -2; // ошибка запроса
+                }
+            }
+            else
+            {
+                return -1; // нет подключения
+            }
+        }
+        // удалить объявление
+        public int DeleteAdvert(string user_id, string advert_id)
+        {
+            if (this.IsConnected)
+            {
+                this.client.Send("<delete_advert/" + user_id + "/" + advert_id + ">");
+                this.result = this.client.Receive();
+
+                if(this.result.Contains("<delete_advert/ok>"))
+                {
+                    return 0; // успешно
+                }
+                else
+                {
+                    if(this.result.Contains("<delete_advert/bad>"))
+                    {
+                        return -2; // ответ о неудаче
+                    } 
+                    else
+                    {
+                        return -3; // неизвестная ошибка
+                    }
+                }
+            }
+            else
+            {
+                return -1; // нет соединения
+            }
+        }
+        // пожаловаться на объявление
+        public int BadAdvert(string user_id, string advert_id)
+        {
+            if (this.IsConnected)
+            {
+                this.client.Send("<bad_advert/" + user_id + "/" + advert_id + ">");
+                this.result = this.client.Receive();
+
+                if(this.result.Contains("<bad_advert/ok>"))
+                {
+                    return 0; // успех
+                }
+                else
+                {
+                    return -2; // неизвестная ошибка
+                }
+            }
+            else
+            {
+                return -1; // нет соединения
+            }
+        }
+        // количество объвлений пользователя
+        public int NumberOfUserAds(string user_id)
+        {
+            if (this.IsConnected)
+            {
+                this.client.Send("<number_of_user_ads/" + user_id + ">");
+                this.result = this.client.Receive();
+                
+                if(this.result.Contains("<number_of_user_ads/"))
+                {
+                    if(this.result.Contains("<number_of_user_ads/bad>"))
+                    {
+                        return -3; // ошибка запроса
+                    }
+                    else
+                    {
+                        return Int32.Parse(this.result.Split(new Char[] { '/', '>' })[1]); // возвращаем количество объвлений
+                    }
+                }
+                else
+                {
+                    return -2; // неизвестная ошибка
+                }
+            }
+            else
+            {
+                return -1; // нет соединения
+            }
+        }
+        // получить 30 объявлений пользователя, последним из которых будет
+        // объявление с id, принимаемым параметром метода
+        public string ListUserAds(string user_id, string number_of_ad)
+        {
+            if (this.IsConnected)
+            {
+                this.client.Send("<list_user_ads/" + user_id + "/" + number_of_ad + ">");
+                this.result = this.client.Receive();
+
+                if (this.result.Contains("<list_user_ads/"))
+                {
+                    string[] ads = result.Split(new Char[] { '/', '<', '>' });
+
+                    // флажок нужен для пропуска первого захода
+                    bool flag = true;
+                    string str = "";
+
+                    // в цикле заполняем список объявлений
+                    foreach (string s in ads)
+                    {
+                        // если строка не пустая
+                        if (s.Trim() != "")
+                        {
+                            // исключаем первый заход
+                            if (flag != true)
+                            {
+                                str = str + s + "/";
+                            }
+                            else
+                            {
+                                // смена флага
+                                flag = false;
+                            }
+                        }
+                    }
+                    return str; // #|ad1/#|ad2/../#|adn/
+                }
+                else
+                {
+                    return null; // неизвестная ошибка
+                }
+            }
+            else
+            {
+                return null; // отсутствует подключение
+            }
         }
     }
 }
